00f612aa1c367cea408ed9dfa2c6520eb0d9ef9a
David P. Riedel <davidp.riedel@gmail.com>
2021-03-08 12:15:50 -0500

    Most of this is auto generated.

---
+++ README.md
@@ -0,0 +1,23 @@
+# coc-ltls
+
+extension to for languagetool_languageserver
+
+## Install
+
+`:CocInstall coc-ltls`
+
+## Keymaps
+
+`nmap <silent> <C-l> <Plug>(coc-coc-ltls-keymap)`
+
+## Lists
+
+`:CocList demo_list`
+
+## License
+
+MIT
+
+---
+
+> This extension is built with [create-coc-extension](https://github.com/fannheyward/create-coc-extension)
---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { LanguageClientOptions, ExtensionContext, LanguageClient, services, workspace } from 'coc.nvim';
+
+export async function activate(context: ExtensionContext): Promise<void> {
+  const config = workspace.getConfiguration('ltls');
+  const isEnable = config.get<boolean>('enabled', true);
+  if (!isEnable) {
+    return;
+  }
+
+  const serverOptions = {
+    command: '/home/dpriedel/projects/languagetool_languageserver/__main__.py', // run jls
+  };
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: ['text'], // run ltls on text files
+  };
+  const client = new LanguageClient(
+    'ltls', // the id
+    'languagetool', // the name of the language server
+    serverOptions,
+    clientOptions
+  );
+  context.subscriptions.push(services.registLanguageClient(client));
+  client.onReady().then(() => {
+    if (!config.get<boolean>('startupMessage', false)) {
+      return;
+    }
+    workspace.showMessage(`ltls: running `);
+  });
+}
